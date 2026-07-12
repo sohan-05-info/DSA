9a4e6dbd8d844ff154ac0142b24bbc3a4524ec44
sohan-05-info <sohanp9105@gmail.com>
2026-07-12 20:46:21 +0530

    Added another Two Sum solution

---
+++ Two_sum_2.cpp
@@ -0,0 +1,26 @@
+/*Problem: Two Sum II - Input Array Is Sorted*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i = 0;
+        int j = numbers.size() - 1;
+
+        while(i < j) {
+            int sum = numbers[i] + numbers[j];
+            if(sum==target) {
+                return{i+1, j+1};
+            }
+            else if(sum<target) {
+                i++;
+            }
+            else {
+                j--;
+            }
+        }
+        return{};
+    }
+};
