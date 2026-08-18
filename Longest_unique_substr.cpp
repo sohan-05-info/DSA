448a4b594ceeee6638be42724b56d8d64f7717b2
sohan-05-info <sohanp9105@gmail.com>
2026-08-18 21:09:46 +0530

    Added the Longest substring without Repeating characters solution

---
+++ Longest_unique_substr.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int low = 0;
+        int high = 0;
+        int n = s.size();
+        int res = INT_MIN;
+        unordered_map<char,int> f;
+        for(high = 0; high < n; high++) {
+            f[s[high]]++;
+            int k = high - low + 1;
+            while(f.size() < k) {
+                f[s[low]]--;
+                if(f[s[low]] == 0)
+                f.erase(s[low]);
+                low++;
+                k = high - low + 1;
+            }
+
+            int len = high - low + 1;
+            res = max(res,len);
+        }
+
+        if(res == INT_MIN)
+        return 0;
+        return res;
+    }
+};
