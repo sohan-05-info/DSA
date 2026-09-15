0d8c0fa0288ce9014683cc40f011df3e0d148ff5
sohan-05-info <sohanp9105@gmail.com>
2026-09-15 14:46:12 +0530

    Add Longest Repeating Character Replacement solution

---
+++ Long_rep_char_replace.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int find(vector<int> &a) {
+        int maxc = -1;
+        for(int i = 0; i < 256; i++)
+        maxc = max(maxc, a[i]);
+        return maxc;
+        }
+
+    int characterReplacement(string s, int k) {
+        int n = s.size();
+        vector <int> f(256,0);
+        int low = 0;
+        int high = 0;
+        int res = INT_MIN;
+        for(high = 0; high < n; high++) {
+            f[s[high]]++;
+            int maxcnt = find(f);
+            int len = high - low + 1;
+            int diff = len - maxcnt;
+
+            while(diff > k) {
+                f[s[low]]--;
+                low++;
+                maxcnt = find(f);
+                len = high - low + 1;
+                diff = len - maxcnt;
+            }
+            len = high - low + 1;
+            res = max(res, len);
+
+        }
+        return res;
+
+    }
+};
